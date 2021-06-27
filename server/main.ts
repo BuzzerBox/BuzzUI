3eae04ae676c360a20428e5dc48cc441600fc0c7
hexxag0n <hexxag0n.developer@gmail.com>
2021-06-27 12:50:17 +0200

    introdiced i2c service

--- server/main.ts
+++ server/main.ts
@@ -1,6 +1,8 @@
 import config from '../config.json';
 import {HttpServerService} from "./services/http-server.service";
 import {GameService} from "./services/game.service";
+import {I2cService} from "./services/i2c.service";
 
 HttpServerService.get().start(config.server.port);
 GameService.startGame();
+I2cService.get().test();
