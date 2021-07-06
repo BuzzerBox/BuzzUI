38dceca365e2b97852bcb4bdfe49b4ec31bb5f4e
hexxag0n <hexxag0n.developer@gmail.com>
2021-07-06 10:29:46 +0200

    broke circular dependency

---
+++ shared/enums.ts
@@ -0,0 +1,11 @@
+import {EPacketTypes} from './enums/EPacketTypes';
+import {EGameStates} from './enums/EGameStates';
+import {EKeyBinds} from './enums/EKeyBinds';
+import {EAnswerStates} from './enums/EAnswerStates';
+
+export {
+	EPacketTypes,
+	EGameStates,
+	EKeyBinds,
+	EAnswerStates
+}
