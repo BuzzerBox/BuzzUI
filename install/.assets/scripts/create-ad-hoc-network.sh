e3321374f75ec6bf60e86d5ebe206deb01bf33de
hexxag0n <hexxag0n.developer@gmail.com>
2021-12-05 20:09:47 +0100

    #46 working but needs cleanup

--- install/.assets/scripts/create-ad-hoc-network.sh
+++ install/.assets/scripts/create-ad-hoc-network.sh
@@ -3,6 +3,6 @@ echo "Creating Hotspot"
 ip link set dev wlan0 down
 ip a add 10.0.0.5/24 brd + dev wlan0
 ip link set dev wlan0 up
-dhcpcd -k wlan0 >/dev/null 2>&1
+dhcpcd -k wlan0 > /dev/null 2>&1 &
 systemctl restart dnsmasq
 systemctl restart hostapd
