8ca2ce8ac4a44a66035fbf0e77615ecbc25f47c1
hexxag0n <hexxag0n.developer@gmail.com>
2022-01-09 01:37:54 +0100

    done #69 fixed import for questions, their answers and their media settings

--- shared/shared.ts
+++ shared/shared.ts
@@ -1,6 +1,8 @@
 export * from './enums';
 import {PacketHelper} from './helper/packet.helper';
+export {StringHelper} from './helper/string.helper';
 export * from './interfaces';
+export * from './services/abstract-logger.service'
 
 export {
     PacketHelper
